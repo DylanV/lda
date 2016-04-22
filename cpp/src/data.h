79e9d14a47859c79e7282a041bdabbbd2d69feeb
DylanV <dverrezen@gmail.com>
2016-04-22 12:48:28 +0200

    Settings structs added

--- cpp/src/data.h
+++ cpp/src/data.h
@@ -12,6 +12,25 @@
 #include "lda.h"
 #include <string>
 
+struct lda_settings {
+    double converged_threshold = 1e-4;
+    int min_iterations = 2;
+    int max_iterations = 100;
+    double inference_converged_threshold = 1e-6;
+    int inference_max_iterations = 20;
+};
+
+struct alpha_settings {
+    bool singular = true;
+    double newton_threshold = 1e-5;
+    int max_iterations = 1000;
+};
+
+struct file_settings {
+    char param_sep = ' ';
+    char param_nl = '\n';
+};
+
 //! load a corpus from a file
 doc_corpus load_corpus(std::string file_path);
 //! split a string with the given delimiter
