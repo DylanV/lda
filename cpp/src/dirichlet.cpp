b68ae137d5e7ab7bf87e0ee87ef533b8cd25eeb6
DylanV <dverrezen@gmail.com>
2016-05-18 16:00:49 +0200

    Added constructors for dirichlet class

--- cpp/src/dirichlet.cpp
+++ cpp/src/dirichlet.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "dirichlet.h"
+
+dirichlet::dirichlet(double init_prec, std::vector<double> init_mean)
+{
+    s = init_prec;
+    mean = init_mean;
+    K = int(mean.size());
+    alpha = std::vector<double>(K);
+    for(int i=0; i<K; i++){
+        alpha[i] = mean[i]*s;
+    }
+}
+
+dirichlet::dirichlet(double init_prec, int K)
+{
+    s = init_prec;
+    dirichlet::K = K;
+    mean = std::vector<double>(K, 1.0/K);
+    for(int i=0; i<K; i++){
+        alpha[i] = mean[i]*s;
+    }
+}
--- cpp/src/dirichlet.h
+++ cpp/src/dirichlet.h
@@ -6,9 +6,25 @@
 #define LDA_DIRICHLET_H
 
 
+#include <vector>
+
 class dirichlet {
+public:
+
+    //! Constructor to start with centered dirichlet
+    dirichlet(double init_prec, int K);
+    //! Constructor with mean specified
+    dirichlet(double init_prec, std::vector<double> init_mean);
+
+    double s;                   /*!< The concentration coefficient or precision of the dirichlet.*/
+    int K;                      /*!< The number of dimensions*/
+    std::vector<double> alpha;  /*!< The alpha psuedo count parameter*/
+    std::vector<double> mean;   /*!< The mean of the dirichlet which is simply alpha / precision*/
+
+private:
 
 };
+
 
 
 #endif //LDA_DIRICHLET_H
