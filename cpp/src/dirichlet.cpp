9975264e360642c812bdbe3f9c11ee8969a43634
DylanV <dverrezen@gmail.com>
2016-05-18 15:08:32 +0200

    Initial commit for dirichlet class

---
+++ cpp/src/dirichlet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dylan on 18/05/2016.
+//
+
+#include "dirichlet.h"
---
+++ cpp/src/dirichlet.h
@@ -0,0 +1,14 @@
+//
+// Created by Dylan on 18/05/2016.
+//
+
+#ifndef LDA_DIRICHLET_H
+#define LDA_DIRICHLET_H
+
+
+class dirichlet {
+
+};
+
+
+#endif //LDA_DIRICHLET_H
