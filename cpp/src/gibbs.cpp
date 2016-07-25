d737385a7066a9c70b78b29870face4b89e1e1ea
DylanV <dverrezen@gmail.com>
2016-07-25 10:38:39 +0200

    Initial commit for gibbs class

---
+++ cpp/src/gibbs.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dylan Verrezen on 2016/07/25.
+//
+
+#include "gibbs.h"
---
+++ cpp/src/gibbs.h
@@ -0,0 +1,14 @@
+//
+// Created by Dylan Verrezen on 2016/07/25.
+//
+
+#ifndef LDA_GIBBS_H
+#define LDA_GIBBS_H
+
+
+class gibbs {
+
+};
+
+
+#endif //LDA_GIBBS_H
