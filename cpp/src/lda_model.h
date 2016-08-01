2ee05757f5c45fbbd9ff868e566e6864db5b09f7
DylanV <dverrezen@gmail.com>
2016-08-01 08:56:51 +0200

    lda_model docs

--- cpp/src/lda_model.h
+++ cpp/src/lda_model.h
@@ -13,18 +13,18 @@
  * A document is a collection of words. Word order does not matter only word frequency is recoreded.
  */
 struct document {
-    std::map<int,int> wordCounts;      /*! Map of word id to word counts. */
-    size_t count;                      /*! The total number of words in the document. */
-    size_t uniqueCount;                /*! The number of unique words (tokens) in the document. */
+    std::map<int,int> wordCounts;      /*!< Map of word id to word counts. */
+    size_t count;                      /*!< Total number of words in the document. */
+    size_t uniqueCount;                /*!< Number of unique words (tokens) in the document. */
 };
 
 /*! Represents a corpus of documents.
  * @sa document
  */
 struct doc_corpus {
-    std::vector<document> docs;    /*! vector of document structs. \sa document */
-    size_t numTerms;               /*! the total number of unique terms in the corpus. */
-    size_t numDocs;                /*! the total number of documents. */
+    std::vector<document> docs;    /*!< Vector of document structs. \sa document */
+    size_t numTerms;               /*!< Total number of unique terms in the corpus. */
+    size_t numDocs;                /*!< Total number of documents. */
 };
 
 /*! Holds all of the settings for the lda model.
@@ -36,21 +36,30 @@ struct lda_settings {
                      inf_max_iterations(20), estimate_alpha(false),
                      alpha_update_interval(1){}
 
-    double converged_threshold;     /*!< The convergence threshold used in training */
-    int min_iterations;             /*!< Minimum number of iterations to train for */
-    int max_iterations;             /*!< Maximum number of iterations to train for */
-    double inf_converged_threshold; /*!< Document inference convergence threshold*/
-    int inf_max_iterations;         /*!< Document inference max iterations*/
-    bool estimate_alpha;            /*!< Whether to estimate alpha*/
-    int alpha_update_interval;      /*!< interval to update alpha on*/
+    double converged_threshold;     /*!< The convergence threshold used in training. */
+    int min_iterations;             /*!< Minimum number of iterations to train for. */
+    int max_iterations;             /*!< Maximum number of iterations to train for. */
+    double inf_converged_threshold; /*!< Document inference convergence threshold. */
+    int inf_max_iterations;         /*!< Document inference max iterations. */
+    bool estimate_alpha;            /*!< Whether to estimate alpha. */
+    int alpha_update_interval;      /*!< Interval to update alpha on. */
 };
 
-/*!
- * Class represent a latent dirichlet allocation model/
+/*! Represetation of latent dirichlet allocation model.
+ * Each of the inference methods inherits from this class so that there is a
+ * common interface between them.
  */
 class lda_model {
 public:
+    /*! Train the model
+     * Trains the model on the corpus given the number of topics
+     * @param [in] numTopics Number of topics to train with/
+     */
     virtual void train(size_t numTopics) = 0;
+    /*! Save the model parameters to files.
+     * Saves the model parameters to files in the given folder. Previous files are overwritten.
+     * @param [in] file_dir Folder directory to write the parameter files.
+     */
     virtual void save_parameters(std::string file_dir) = 0;
 };
 
