ecf01efa68d556b7eb438a9d021d9c30688cdb36
DylanV <dverrezen@gmail.com>
2016-08-24 12:31:18 +0200

    Initial commit for expectaiton propagation

---
+++ cpp/src/expectation_prop.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dylan Verrezen on 2016/08/22.
+//
+
+#include "expectation_prop.h"
---
+++ cpp/src/expectation_prop.h
@@ -0,0 +1,16 @@
+//
+// Created by Dylan Verrezen on 2016/08/22.
+//
+
+#ifndef LDA_EXPECTATION_PROP_H
+#define LDA_EXPECTATION_PROP_H
+
+
+#include "lda_model.h"
+
+class expectation_prop : public lda_model {
+
+};
+
+
+#endif //LDA_EXPECTATION_PROP_H
