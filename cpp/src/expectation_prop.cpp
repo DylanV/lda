02c5552f86dc516b4912670d948ad662d4676cb9
DylanV <dverrezen@gmail.com>
2016-08-24 15:52:21 +0200

    Started skeleton of expectation propagation

--- cpp/src/expectation_prop.cpp
+++ cpp/src/expectation_prop.cpp
@@ -1,5 +1,14 @@
-//
-// Created by Dylan Verrezen on 2016/08/22.
-//
 
 #include "expectation_prop.h"
+
+expectation_prop::expectation_prop(doc_corpus &corp) {
+
+}
+
+void expectation_prop::train(size_t numTopics) {
+
+}
+
+void expectation_prop::save_parameters(std::string file_dir) {
+
+}
--- cpp/src/expectation_prop.h
+++ cpp/src/expectation_prop.h
@@ -1,14 +1,20 @@
-//
-// Created by Dylan Verrezen on 2016/08/22.
-//
 
 #ifndef LDA_EXPECTATION_PROP_H
 #define LDA_EXPECTATION_PROP_H
 
-
 #include "lda_model.h"
 
 class expectation_prop : public lda_model {
+public:
+    //! The constructor.
+    expectation_prop(doc_corpus& corp);
+    //! Train a lda model with the given number of topics.
+    void train(size_t numTopics);
+    //! Save the model parameters to the given folder.
+    void save_parameters(std::string file_dir);
+
+private:
+    doc_corpus corpus;        /*!< The corpus */
 
 };
 
