9a21b13b603999eaaeaa264bced8e529fd40c742
DylanV <dverrezen@gmail.com>
2017-01-26 17:04:36 +0200

    Python wrapper working on osx. Not tested on windows

--- cpp/wrapper.py
+++ cpp/wrapper.py
@@ -5,7 +5,7 @@ from subprocess import Popen, PIPE
 
 
 WINDOWS = True if system() == 'Windows' else False
-WORK_DIR = join(os.getcwd(), 'build')
+WORK_DIR = abspath(join(os.getcwd(), 'build'))
 LDA_PATH = join(os.getcwd(), 'build', 'lda.exe') if WINDOWS else join(os.getcwd(), 'build', 'lda')
 OUT_PATH = join('..','..','param','artificial')
 CORPUS_PATH = join('..','..','datasets','artificial.dat')
@@ -15,16 +15,15 @@ assert isfile(LDA_PATH), ('Unable to find the lda binary in the '
 
 def run_LDA():
     lda_args = (abspath(LDA_PATH),
-            '-c', abspath(OUT_PATH),
+            '-c', CORPUS_PATH,
             '-t', '10',
-            '-o', abspath(OUT_PATH),
+            '-o', OUT_PATH,
             '-i', '3')
 
-    p = Popen(lda_args, stdout=PIPE, stderr=PIPE)
-    # p.wait()
+    p = Popen(lda_args, stdout=PIPE, stderr=PIPE, cwd=WORK_DIR)
+    p.wait()
     out, err = p.communicate()
     print(out)
-    print(err)
     print(p.returncode)
 
 
